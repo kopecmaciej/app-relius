2eee1e75c1d9fbae0cac959848d4ef1d3815de21
Maciej Kopeć <maciejkopec92@gmail.com>
2024-10-24 16:54:41 +0200

    feat: improve language switching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,10 @@ import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { useChangeLocale, useCurrentLocale, useI18n } from "@/locales/client";
 
-
 export default function Navbar() {
   const t = useI18n();
   const changeLocale = useChangeLocale();
   const locale = useCurrentLocale();
-
 
   return (
     <nav className="bg-background text-foreground p-4">
@@ -20,17 +18,18 @@ export default function Navbar() {
         </Link>
         <div className="space-x-4 flex items-center">
           <Button variant="ghost" asChild>
-            <Link href="/about">{t('about')}</Link>
+            <Link href="/about">{t("about")}</Link>
           </Button>
           <Button variant="ghost" asChild>
-            <Link href="/projects">{t('projects')}</Link>
+            <Link href="/projects">{t("projects")}</Link>
           </Button>
           <Button variant="ghost" asChild>
-            <Link href="/contact">{t('contact')}</Link>
+            <Link href="/contact">{t("contact")}</Link>
           </Button>
           <ThemeToggle />
-          <Button onClick={() => changeLocale('en')}>EN</Button>
-          <Button onClick={() => changeLocale('pl')}>PL</Button>
+          <Button onClick={() => changeLocale(locale == "pl" ? "en" : "pl")}>
+            {locale.toUpperCase()}
+          </Button>
         </div>
       </div>
     </nav>
