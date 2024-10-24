bd9a5795b6f7d9a958e6013fbdb6fbceb7665798
Maciej Kopeć <maciejkopec92@gmail.com>
2024-10-24 18:05:45 +0200

    style: Format About page with consistent spacing and alignment.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,18 +1,47 @@
 "use client";
 
 import { useI18n } from "@/locales/client";
+import { Github, Linkedin, LucideIcon, Mail } from "lucide-react";
 
 export default function About() {
   const t = useI18n();
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-8 text-white">
-      <h1 className="text-5xl font-bold mb-4 text-teal-400">{t("aboutTitle")}</h1>
-      <p className="text-xl mb-8 max-w-2xl text-center text-gray-300">{t("aboutDescription")}</p>
+      <h1 className="text-5xl font-bold mb-4 text-teal-400">
+        {t("aboutTitle")}
+      </h1>
+      <p className="text-xl mb-8 max-w-2xl text-center text-gray-300">
+        {t("aboutDescription")}
+      </p>
       <div className="text-lg text-gray-300">
-        <p>Email: <a href="mailto:maciejkopec92@gmail.com" className="text-teal-400 hover:underline">maciejkopec92@gmail.com</a></p>
-        <p>GitHub: <a href="https://github.com/kopecmaciej" className="text-teal-400 hover:underline">kopecmaciej</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/m-kopec" className="text-teal-400 hover:underline">m-kopec</a></p>
+        <p>
+          Email:{" "}
+          <a
+            href="mailto:maciejkopec92@gmail.com"
+            className="text-teal-400 hover:underline"
+          >
+            maciejkopec92@gmail.com
+          </a>
+        </p>
+        <p>
+          GitHub:{" "}
+          <a
+            href="https://github.com/kopecmaciej"
+            className="text-teal-400 hover:underline"
+          >
+            kopecmaciej
+          </a>
+        </p>
+        <p>
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/m-kopec"
+            className="text-teal-400 hover:underline"
+          >
+            m-kopec
+          </a>
+        </p>
       </div>
     </div>
   );
